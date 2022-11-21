36954b65998d2c6fa0836b79adfd974cdc75fc89
Phif <71464584+Phif@users.noreply.github.com>
2022-11-21 13:41:36 +0100

    refacto js

--- js/main.js
+++ js/main.js
@@ -2,66 +2,44 @@ import Sortable from '/node_modules/sortablejs/modular/sortable.complete.esm.js'
 
 $(function() {
     //----- SORTABLE -----//
-    var el = document.getElementById('images-list');
-    new Sortable(el, {
-        animation: 200,
-        swapThreshold: 1,
-        dragClass: "sortable-drag",
-        group: 'list'
-    });
-    
-    var el = document.getElementById('s-tier');
-    new Sortable(el, {
-        animation: 200,
-        swapThreshold: 1,
-        dragClass: "sortable-drag",
-        group: 'list',
-    });
-    
-    var el = document.getElementById('a-tier');
-    new Sortable(el, {
-        animation: 200,
-        swapThreshold: 1,
-        dragClass: "sortable-drag",
-        group: 'list'
-    });
-    
-    var el = document.getElementById('b-tier');
-    new Sortable(el, {
-        animation: 200,
-        swapThreshold: 1,
-        dragClass: "sortable-drag",
-        group: 'list'
-    });
-    
-    var el = document.getElementById('c-tier');
-    new Sortable(el, {
-        animation: 200,
-        swapThreshold: 1,
-        dragClass: "sortable-drag",
-        group: 'list'
-    });
-    
-    var el = document.getElementById('d-tier');
-    new Sortable(el, {
-        animation: 200,
-        swapThreshold: 1,
-        dragClass: "sortable-drag",
-        group: 'list'
+    let sortableElements = new Array('images-list', 's-tier', 'a-tier', 'b-tier', 'c-tier', 'd-tier');
+    sortableElements.forEach(element => {
+        var el = document.getElementById(element);
+        new Sortable(el, {
+            animation: 200,
+            swapThreshold: 1,
+            dragClass: "sortable-drag",
+            group: 'list'
+        });
     });
     //----------//
-    
-    //----- ZOOM ON MOUSECLICK -----//
+
+
+    //----- TOOLTIP -----//
+    $( document ).tooltip({
+        show: {
+            delay: 0
+        },
+        position: {
+            my: "center bottom-30px",
+            at: "center top",
+        },
+        track: true
+    });
+    //----------//
+
+
+    //----- MOUSE EVENTS -----//
     $(document).on("mouseleave", ".tier-images", function() {
         $(this).css('z-index', 0);
     });
-    
+
     $(document).on("mousedown", ".tier-images", function() {
         $(this).css('transition', 'transform ease-in-out 0.2s');
         $(this).css('transform', 'scale(' + 2 + ')');
         $(this).css('z-index', 2);
     });
-    
+
     $(document).on("mouseup", ".tier-images", function() {
         $(this).css('transition', 'all ease-in-out 0.2s');
         $(this).css('transform', 'scale(' + 1 + ')');
@@ -69,17 +47,6 @@ $(function() {
     });
     //----------//
     
-    //----- TOOLTIP -----//
-    $( document ).tooltip({
-        show: {
-            delay: 500
-        },
-        position: {
-            my: "center bottom-20px",
-            at: "center top",
-        },
-        // track: true
-    });
     
     //----- ADD IMAGES -----//
     document.querySelector("#input-file").addEventListener("change", (e) => { 
@@ -109,70 +76,22 @@ $(function() {
 
 
     //----- DOUBLE CLICK TO EDIT -----//
-    $(document).on("dblclick", "#edit-tier-s", function(){
-        var current = $(this).text();
-        $("#edit-tier-s").html(`<textarea class="form-control" id="newcont" style="width:100px;">${current}</textarea>`);
-        $("#newcont").focus();
-        
-        $("#newcont").focus(function() {
-            console.log('in');
-        }).blur(function() {
-             var newcont = $("#newcont").val();
-             $("#edit-tier-s").text(newcont);
-        });
-    })
-
-    $(document).on("dblclick", "#edit-tier-a", function(){
-        var current = $(this).text();
-        $("#edit-tier-a").html(`<textarea class="form-control" id="newcont" style="width:100px;">${current}</textarea>`);
-        $("#newcont").focus();
-        
-        $("#newcont").focus(function() {
-            console.log('in');
-        }).blur(function() {
-            var newcont = $("#newcont").val();
-            $("#edit-tier-a").text(newcont);
-        });
-    })
-
-    $(document).on("dblclick", "#edit-tier-b", function(){
-        var current = $(this).text();
-        $("#edit-tier-b").html(`<textarea class="form-control" id="newcont" style="width:100px;">${current}</textarea>`);
-        $("#newcont").focus();
-        
-        $("#newcont").focus(function() {
-            console.log('in');
-        }).blur(function() {
-             var newcont = $("#newcont").val();
-             $("#edit-tier-b").text(newcont);
-        });
-    })
-
-    $(document).on("dblclick", "#edit-tier-c", function(){
-        var current = $(this).text();
-        $("#edit-tier-c").html(`<textarea class="form-control" id="newcont" style="width:100px;">${current}</textarea>`);
-        $("#newcont").focus();
-        
-        $("#newcont").focus(function() {
-            console.log('in');
-        }).blur(function() {
-             var newcont = $("#newcont").val();
-             $("#edit-tier-c").text(newcont);
-        });
-    })
-
-    $(document).on("dblclick", "#edit-tier-d", function(){
-        var current = $(this).text();
-        $("#edit-tier-d").html(`<textarea class="form-control" id="newcont" style="width:100px;">${current}</textarea>`);
-        $("#newcont").focus();
-        
-        $("#newcont").focus(function() {
-            console.log('in');
-        }).blur(function() {
-             var newcont = $("#newcont").val();
-             $("#edit-tier-d").text(newcont);
-        });
-    })
+    let tierTitles = new Array("#edit-tier-s", "#edit-tier-a", "#edit-tier-b", "#edit-tier-c", "#edit-tier-d");
+    tierTitles.forEach(element => {
+        $(document).on("dblclick", element, function(){
+            var current = $(this).text();
+            $(element).html(`<textarea class="form-control" id="newcont" style="width:100px; height:100px;">${current}</textarea>`);
+            $("#newcont").focus();
+            
+            $("#newcont").focus(function() {
+                console.log('in');
+            }).blur(function() {
+                 var newcont = $("#newcont").val();
+                 $(element).text(newcont);
+            });
+        })
+    });
     //----------//
+    
 });
 
