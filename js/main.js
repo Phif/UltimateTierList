a8a4ed89696ddc13cc4cfae4564cd603b50e6b10
Phif <71464584+Phif@users.noreply.github.com>
2022-11-29 02:37:38 +0100

    added new tiers + refacto + fixed tier options

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@ import Sortable from '/node_modules/sortablejs/modular/sortable.complete.esm.js'
 
 $(function() {
     //----- SORTABLE -----//
-    var el = document.querySelector('.tier-list');
+    
+    var el = document.querySelector('#tier-list');
     new Sortable(el, {
         animation: 200,
         swapThreshold: 1,
+        handle: ".handle"
     });
     
-    let sortableElements = new Array('images-list', 's-tier', 'a-tier', 'b-tier', 'c-tier', 'd-tier');
-    sortableElements.forEach(element => {
+    let imageSortable = document.querySelectorAll(".images-sort");
+    let imageSortableArray = new Array;
+    for (let i = 0; i < imageSortable.length; i++) {
+        imageSortableArray.push(imageSortable[i].getAttribute("id"));
+    }
+
+    imageSortableArray.forEach(element => {
         var el = document.getElementById(element);
         new Sortable(el, {
             animation: 200,
@@ -82,21 +89,32 @@ $(function() {
     
     //----- DOUBLE CLICK TO EDIT -----//
     // Tier title
-    let tierTitles = new Array("#edit-tier-s", "#edit-tier-a", "#edit-tier-b", "#edit-tier-c", "#edit-tier-d");
-    tierTitles.forEach(element => {
-        $(document).on("dblclick", element, function(){
-            var current = $(this).text();
-            $(element).html(`<textarea class="form-control" id="newcont" style="width:100px; height:100px;">${current}</textarea>`);
-            $("#newcont").focus();
-            $("#newcont").focus().blur(function() {
-                var newcont = $("#newcont").val();
-                $(element).text(newcont);
-            });
-        })
-    });
+    let tierTitles = document.querySelectorAll(".tier-title");
+    let tierTitlesArray = new Array;
+    updateTierTitles();
+    function updateTierTitles() {
+        tierTitles = document.querySelectorAll(".tier-title");
+        tierTitlesArray = new Array;
+
+        for (let i = 0; i < tierTitles.length; i++) {
+            tierTitlesArray.push("#" + tierTitles[i].getAttribute("id"));
+        }
+        
+        tierTitlesArray.forEach(element => {
+            $(document).on("click", element, function(){
+                var current = $(this).text();
+                $(element).html(`<textarea class="form-control" id="newcont" style="width:100px; height:100px;">${current}</textarea>`);
+                $("#newcont").focus();
+                $("#newcont").focus().blur(function() {
+                    var newcont = $("#newcont").val();
+                    $(element).text(newcont);
+                });
+            })
+        });
+    }
     
     // Tierlist title
-    $(document).on("dblclick", "#tier-list-title", function(){
+    $(document).on("click", "#tier-list-title", function(){
         var current = $(this).text();
         $("#tier-list-title").html(`<input class="form-control" id="newcont" placeholder="${current}" style="width:100%"></input>`);
         $("#newcont").focus();
@@ -113,14 +131,64 @@ $(function() {
     
     /* COLOR PICKER */
     let colorPickers = document.querySelectorAll(".color-picker");
-    colorPickers.forEach(element => {
-        element.addEventListener("input", function(){ 
-            element.parentElement.style.backgroundColor = event.target.value;
-         });
-    });
+    updateColorPickers();
+    function updateColorPickers() {
+        colorPickers = document.querySelectorAll(".color-picker");
+        colorPickers.forEach(element => {
+            element.addEventListener("input", function(){ 
+                element.parentElement.style.backgroundColor = event.target.value;
+            });
+        });
+    }
     //----------//
+    
+    /* ADD NEW TIER */
+    let divTierNumber = 5;
+    
+    // Default colors
+    document.querySelector("#tier-title-0").parentElement.style.backgroundColor = "#b20022";
+    document.querySelector("#tier-title-1").parentElement.style.backgroundColor = "#ff7f00";
+    document.querySelector("#tier-title-2").parentElement.style.backgroundColor = "#e5e52e";
+    document.querySelector("#tier-title-3").parentElement.style.backgroundColor = "#71dc56";
+    document.querySelector("#tier-title-4").parentElement.style.backgroundColor = "#5eb1d4";
+    document.querySelector("#tier-title-5").parentElement.style.backgroundColor = "#d45eba";
 
-    /* ADD TIER */
+    let buttonAddTier = document.querySelector("#add-tier");
+    buttonAddTier.addEventListener("click", function() {
+        divTierNumber += 1;
+        let divTier = document.createElement("div");
+        divTier.setAttribute("class", "tier");
+        divTier.setAttribute("id", `tier-${divTierNumber}`);
+        divTier.setAttribute("style", "background-color : #ccc;");
+        
+        let spanIcon = document.createElement("span");
+        spanIcon.setAttribute("class", "material-symbols-outlined handle");
+        spanIcon.innerText = "drag_indicator";
+        
+        let inputColorPicker = document.createElement("input");
+        inputColorPicker.setAttribute("type", "color");
+        inputColorPicker.setAttribute("value", "#ffffff");
+        inputColorPicker.setAttribute("class", "color-picker");
+        
+        let divTierTitle = document.createElement("div");
+        divTierTitle.setAttribute("class", "tier-title");
+        divTierTitle.setAttribute("id", `tier-title-${divTierNumber}`);
+        divTierTitle.innerText = "New Tier";
+        
+        let divTierImages = document.createElement("div");
+        divTierImages.setAttribute("class", "image-sort");
+        
+        let divTierList = document.querySelector("#tier-list");
+        
+        divTierList.appendChild(divTier);
+        divTier.appendChild(spanIcon);
+        divTier.appendChild(inputColorPicker);
+        divTier.appendChild(divTierTitle);
+        divTier.appendChild(divTierImages);
+        
+        updateColorPickers();
+        updateTierTitles();
+    });
 
 });
 
