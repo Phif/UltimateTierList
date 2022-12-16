7747e2010bfda68b9620cb24af0654da08256401
Phif <71464584+Phif@users.noreply.github.com>
2022-12-16 03:10:12 +0100

    feat: uuid generator + removed library

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(function() {
+    //----- UUID -----//
+    function createUUID(){
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (dt + Math.random()*16)%16 | 0;
+            dt = Math.floor(dt/16);
+            return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
+
     //----- SHOW/HIDE CAPTIONS -----//
     var isCaptionOn = false;
     toggleCaptionOff();
@@ -154,13 +165,13 @@ $(function() {
             $(this).css('border', '5px solid mediumturquoise');
             $(this).css('color', 'mediumturquoise');
         },
-        mouseleave: function() {
-            $(this).css('border', '0px solid transparent');
-            $(this).css('color', 'black');
-        },
         dragover: function() {
             $(this).css('border', '5px solid mediumturquoise');
             $(this).css('color', 'mediumturquoise');
+        },
+        mouseleave: function() {
+            $(this).css('border', '0px solid transparent');
+            $(this).css('color', 'black');
         },
         dragleave: function() {
             $(this).css('border', '0px solid transparent');
